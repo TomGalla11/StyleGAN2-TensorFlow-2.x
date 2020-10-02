ef6be787d478ea7971dc4216dc0b42916444daa2
TomGalla11 <67208362+TomGalla11@users.noreply.github.com>
2020-10-02 19:27:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 weights_dir = 'weights/'
 result_dir = 'results'
 model_name = 'ffhq'
+available_weights = ['ffhq', 'car', 'cat', 'horse']
 data_dir = 'datasets'
 cache_dir = 'cache'
 run_dir_ignore = ['results', 'datasets', 'cache']
